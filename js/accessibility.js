5deeb9840368d0de0e03db6d7e55825db8f8e6ee
ben shaya <benshaya@bens-MacBook-Pro-2.local>
2025-12-22 00:28:10 +0200

    Refactor accessibility reset button to match grid style and update UI layout

--- js/accessibility.js
+++ js/accessibility.js
@@ -102,7 +102,10 @@
                             <svg class="acc-ignore" viewBox="0 0 24 24"><path d="M19 13H5v-2h14v2z"/></svg>
                             מדריך קריאה
                         </div>
-                        <div class="acc-reset-btn acc-ignore" id="acc-reset">איפוס הגדרות</div>
+                        <div class="acc-btn acc-ignore acc-reset-btn" id="acc-reset">
+                            <svg class="acc-ignore" viewBox="0 0 24 24"><path d="M17.65 6.35C16.2 4.9 14.21 4 12 4c-4.42 0-7.99 3.58-7.99 8s3.57 8 7.99 8c3.73 0 6.84-2.55 7.73-6h-2.08c-.82 2.33-3.07 4-5.65 4-3.31 0-6-2.69-6-6s2.69-6 6-6c1.66 0 3.14.69 4.22 1.78L13 11h7V4l-2.35 2.35z"/></svg>
+                            איפוס הגדרות
+                        </div>
                     </div>
                 </div>
             </div>
